df0527ef17d8bf9cd7f38200fd4daeb2f63c6e39
PedroIvoMarques <43143773+PedroIvoMarques@users.noreply.github.com>
2018-09-10 14:09:38 -0300

    Add files via upload

---
+++ Ex01-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(void){
+	int a1,a2;
+//---------------------------------//var
+printf ("Digite a1 depois a2: ");
+scanf("%d %d", &a1,&a2);
+if (a1 > a2){
+	printf("Maior e %d\n", a1);}
+else{
+	printf("Maior e %d\n", a2);}
+return 0; 
+}
