cc68fbbcb1f61acc9688cd6bb59a9ef3ea395d1b
PedroIvoMarques <43143773+PedroIvoMarques@users.noreply.github.com>
2018-09-24 09:12:32 -0500

    Programa "ehprimo" v2 - 24/09/2018

--- ex4.1/primo.c
+++ ex4.1/primo.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int eh_primo(int a){
-int cont;
+	int cont=0;
 	for(int i=2; i <= a; i++){
 		if ((a/i)*i==a){
-			cont++;}
+			cont++;
+		}
 	}
 	if (cont==1){
-		return 1;}
+		return 1;
+	}
 	else{
 		return 0;
-}
+	}
 }		
 		
 int main(){
-
 	int a,b;
 	scanf("%d",&a);
 	b=eh_primo(a);
-	printf("%d/n",b);
+	printf("%d\n",b);
 
 }
