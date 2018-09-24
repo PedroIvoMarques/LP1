85cb1f319d10e51e801e48ab1120f3abd53eaf23
PedroIvoMarques <43143773+PedroIvoMarques@users.noreply.github.com>
2018-09-24 10:08:28 -0300

    Add files via upload

---
+++ ex4.1/primo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int eh_primo(int a){
+int cont;
+	for(int i=2; i <= a; i++){
+		if ((a/i)*i==a){
+			cont++;}
+	}
+	if (cont==1){
+		return 1;}
+	else{
+		return 0;
+}
+}		
+		
+int main(){
+
+	int a,b;
+	scanf("%d",&a);
+	b=eh_primo(a);
+	printf("%d/n",b);
+
+}
