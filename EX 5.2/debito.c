1ef56dfb8a0fb209fee94cc7888eeadd958fafb8
PedroIvoMarques <43143773+PedroIvoMarques@users.noreply.github.com>
2018-09-26 08:50:17 -0500

    Exercicio 5.2 - Ponteiros Débito

---
+++ EX 5.2/debito.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+void compra (int* conta, int valor) {
+	*conta=*conta-valor;
+}		
+
+
+int main(){
+	int minha_conta1,minha_conta2 ;int *banco;
+	
+	scanf("%d %d",&minha_conta1,&minha_conta2);
+		
+	if (minha_conta1>minha_conta2){
+
+		banco=&minha_conta1;
+	
+		}
+	else{
+		banco=&minha_conta2;
+		}
+		compra(banco,500);
+		
+	printf("%d %d\n",minha_conta1,minha_conta2);
+	printf("%d\n",*banco);
+return 0;
+}
