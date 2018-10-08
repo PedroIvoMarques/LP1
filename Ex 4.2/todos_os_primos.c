607729241048758797d4b3c4bd3c5e4d8d754fbc
PedroIvoMarques <43143773+PedroIvoMarques@users.noreply.github.com>
2018-10-08 10:03:03 -0300

    Update todos_os_primos.c

--- Ex 4.2/todos_os_primos.c
+++ Ex 4.2/todos_os_primos.c
@@ -6,20 +6,12 @@
 int todos_os_primos(int a){
 	int cont=0;
 	for(int i=2; i <= a; i++){
-		//printf("DDD");
-		//printf("%d %d\n", (a/i)*i, a);
 		if ((a/i)*i==a){
 			cont++;
 		}
 	}
-	if (cont==1){
-		return a ;
-		
-	}
-	else{
-		return 0;
-	}
-}		
+	return cont==1;
+}
 		
 int main(){
 	int a,b,x=2;
@@ -27,7 +19,7 @@ int main(){
 	while(x<=a){
 		b=todos_os_primos(x);
 		if (b!=0){	
-			printf("%d\n", b);
+			printf("%d\n", x);
 		}	
 		x++;
 	}
