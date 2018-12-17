0c8930851775d07dfe7a7bc0bbdf029384f62761
PedroIvoMarques <43143773+PedroIvoMarques@users.noreply.github.com>
2018-12-17 01:25:57 -0200

    struct dentro de struct (navio)

---
+++ Ex. 7.2/Ex. 7.2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Rota{
+	char rota[30];
+	float distancia;
+	int dias;
+};
+
+struct Navio{
+	char nome[20];
+	int comprimento;
+	int boca;
+	float velocidade;
+	struct Rota caminho;
+
+};
+
+
+void preenche(struct Navio* n){
+	scanf("%s",(*n).nome);
+	scanf("%d",&(*n).comprimento);
+	scanf("%d",&(*n).boca);
+	scanf("%f",&(*n).velocidade);
+	scanf("%s",(*n).caminho.rota);
+	scanf("%f",&(*n).caminho.distancia);
+	scanf("%d",&(*n).caminho.dias);
+	
+}
+	
+
+int main (void){
+	struct Navio maru;
+	preenche(&maru);
+	printf("Nome=%s\ncomprimento=%d\nboca=%d\nvelocidade%f\nRealiza a rota %s , distancia nominal de %f em %d dias",maru.nome,maru.comprimento,maru.boca,maru.velocidade,maru.caminho.rota,maru.caminho.distancia,maru.caminho.dias);
+	return 0;
+}
+
